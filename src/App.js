b1bf5811af0de3784b501076df9db9c8e471f145
Md. Mahbub Hossain <mahbubh0ssain.dev@gmail.com>
2023-02-25 00:37:59 +0600

    appbar created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from "./logo.svg";
-import "./App.css";
-
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
 const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="lg">
+      <AppBar position="static" color="inherit">
+        <Typography variant="h2" align="center">
+          Memories
+        </Typography>
+        <img
+          src="https://i.ibb.co/NyJqJWK/memories-Logo.png"
+          alt=""
+          // height="60"
+        />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justifyContent="space-between"
+            alignItems="stretch"
+            spacing={3}
+          >
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   );
 };
 
